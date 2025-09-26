4bdf1cf3b12f85417112d3dea48d643829acac30
chebscut <121395008+chebscut@users.noreply.github.com>
2025-09-26 08:34:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,4 +68,10 @@ def main():
 
     # Настраиваем бота
     app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler_
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("list", list_notes))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
